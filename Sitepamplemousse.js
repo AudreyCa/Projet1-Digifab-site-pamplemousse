36a89723c6f2f16712c33e01ff9b3e8c0842812c
Audrey Cannesson <you@example.coma.cannesson5@gmail.comgit config --global user.name Your>
2022-06-30 18:12:49 +0200

    Ajout du jeudi soir

--- Sitepamplemousse.js
+++ Sitepamplemousse.js
@@ -1,19 +1,62 @@
-let frise1 = document.querySelector('.frise1');
-let frise2 = document.querySelector('.frise2');
+// Animation de la frise
 
+const percentLabel = document.querySelector("#percent");
+const originalTitle = document.title;
+const frise = document.querySelector(".frise");
+let oldpercentage=0;
+let translation = -0.1;
 
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+  let docHeight = document.body.offsetHeight;
+  let winHeight = window.innerHeight;
+  let scrollPercent = scrollTop / (docHeight - winHeight);
+  let scrollPercentRounded = Math.round(scrollPercent * 100);
 
-frise1.addEventListener('scroll', function(e) {
-    frise1.style.transfrom = 'translateX(45%)';
-    console.log(frise1);
+  if (oldpercentage > scrollPercentRounded){
+  console.log(scrollPercentRounded,translation,frise);
+
+  translation ++;
+  frise.setAttribute('style','transform:translateX('+translation+'%);') ;
+  } 
+
+  if (oldpercentage < scrollPercentRounded){
+    console.log(scrollPercentRounded,translation,frise);
+  
+    translation --;
+    frise.setAttribute('style','transform:translateX('+translation+'%);') ;
+    }
+
+  oldpercentage = scrollPercentRounded;
+
 });
 
-frise2.addEventListener('scroll', function(e) {
-    frise2.style.transform = 'translateX(-45%)';
-    console.log(frise2);
+
+
+// Animation paiment
+
+let moins = document.querySelector(".moins");
+let plus = document.querySelector(".plus");
+let nombrePlaceInput = document.querySelector(".number");
+let prixTotalInput = document.querySelector(".total");
+let nombrePlace = 0;
+let prixTotal = 0;
+
+
+
+moins.addEventListener('click', function(e) {
+    if (nombrePlace != 0) {
+      nombrePlace --;
+      nombrePlaceInput.value = nombrePlace;
+      prixTotal -= 10;
+      prixTotalInput.value = prixTotal;
+    }
 });
 
+plus.addEventListener('click', function(e) {
+  nombrePlace ++;
+  nombrePlaceInput.value = nombrePlace;
+  prixTotal += 10;
+  prixTotalInput.value = prixTotal;
+});
 
-// pour entrer le chiffre du nombre de place : Attention O ou +
-
-// Attention : 
